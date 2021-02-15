94b895f35626935ebab8947ab14b580472a8b942
Walentin <valession@gmail.com>
2021-02-16 00:00:36 +0300

    Comments for methods

--- app/src/main/java/com/application/testlifeactivity/MainActivity.java
+++ app/src/main/java/com/application/testlifeactivity/MainActivity.java
@@ -9,10 +9,12 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     public static final String TAG = "StartActivity";
-    TextView logArea;
+    TextView logArea; //логгер в приложении
 
     private Integer count = 5;
-
+    /**
+     * Вызывается при первом запуске приложения, создает activity
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,9 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "onCreate() executed", Toast.LENGTH_LONG).show();
 
     }
-
+    /**
+     * Вызывается при запуске приложения, после создания ее activity
+     */
     @Override
     protected void onStart() {
         super.onStart();
@@ -32,7 +36,9 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "onStart() executed", Toast.LENGTH_LONG).show();
         logArea.setText(logArea.getText() + "\nOnStart()");
     }
-
+    /**
+     * Вызывается при выходе из другого состояния (свернутое и приостановленное состояние)
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -40,7 +46,9 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "onResume() executed", Toast.LENGTH_LONG).show();
         logArea.setText(logArea.getText() + "\nOnResume()");
     }
-
+    /**
+     * Вызывается при сворачивании приложения
+     */
     @Override
     protected void onPause() {
         super.onPause();
@@ -48,7 +56,9 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "onPause() executed", Toast.LENGTH_LONG).show();
         logArea.setText(logArea.getText() + "\nOnPause()");
     }
-
+    /**
+     * Вызывается при остановке activity, сворачивании приложения (сохранить активность)
+     */
     @Override
     protected void onStop() {
         super.onStop();
@@ -56,7 +66,9 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "onStop() executed", Toast.LENGTH_LONG).show();
         logArea.setText(logArea.getText() + "\nOnStop()");
     }
-
+    /**
+     * Вызывается при уничтожении activity
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -65,6 +77,9 @@ public class MainActivity extends Activity {
         logArea.setText(logArea.getText() + "\nOnRestart()");
     }
 
+    /**
+     * Вызывается при уничтожении activity
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -73,6 +88,10 @@ public class MainActivity extends Activity {
         logArea.setText(logArea.getText() + "\nOnDestroy()");
     }
 
+    /**
+     * Вызывается, когда нужно сохранить activity
+     * @param outState - текущее состояние
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -81,6 +100,11 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "Instance state has been saved.", Toast.LENGTH_LONG).show();
         logArea.setText(logArea.getText() + "\nInstance state has been saved.");
     }
+
+    /**
+     * Восстанавливаем состояние
+     * @param savedInstanceState - сохраненное состояние
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -94,6 +118,9 @@ public class MainActivity extends Activity {
 
     }
 
+    /**
+     * Обновляем пользовательский интерфейс
+     */
     private void resetUI() {
         ((TextView) findViewById(R.id.txt_counter)).setText(count.toString());
         Log.d(TAG, "resetUI");
