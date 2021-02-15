f7cf458a34b3feff824da8b8ec9ef7e20a00841e
Walentin <valession@gmail.com>
2021-02-15 23:49:54 +0300

    Work has finished

--- app/src/main/java/com/application/testlifeactivity/MainActivity.java
+++ app/src/main/java/com/application/testlifeactivity/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     public static final String TAG = "StartActivity";
+    TextView logArea;
 
     private Integer count = 5;
 
@@ -15,7 +17,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logArea = findViewById(R.id.logArea);
+        logArea.setText(logArea.getText() + "\nOnCreate()");
         Log.d(TAG, "onCreate");
+        Toast.makeText(this, "onCreate() executed", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
@@ -23,36 +29,48 @@ public class MainActivity extends Activity {
         super.onStart();
         Log.d(TAG, "onStart");
         resetUI();
+        Toast.makeText(this, "onStart() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnStart()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
+        Toast.makeText(this, "onResume() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
+        Toast.makeText(this, "onPause() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnPause()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
+        Toast.makeText(this, "onStop() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnStop()");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
+        Toast.makeText(this, "onRestart() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnRestart()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        Toast.makeText(this, "onDestroy() executed", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nOnDestroy()");
     }
 
     @Override
@@ -60,8 +78,9 @@ public class MainActivity extends Activity {
         super.onSaveInstanceState(outState);
         outState.putInt("count", count);
         Log.d(TAG, "onSaveInstanceState");
+        Toast.makeText(this, "Instance state has been saved.", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nInstance state has been saved.");
     }
-
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -70,11 +89,15 @@ public class MainActivity extends Activity {
             count = savedInstanceState.getInt("count");
         }
         Log.d(TAG, "onRestoreInstanceState");
+        Toast.makeText(this, "Instance state has been restored.", Toast.LENGTH_LONG).show();
+        logArea.setText(logArea.getText() + "\nInstance state has been restored.");
+
     }
 
     private void resetUI() {
         ((TextView) findViewById(R.id.txt_counter)).setText(count.toString());
         Log.d(TAG, "resetUI");
+        Toast.makeText(this, "resetUI", Toast.LENGTH_LONG).show();
     }
 
 
